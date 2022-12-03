c25eb0fdcaef980f72f524005e02f566a8ebebaa
Maxwell O'Roark <m.d.oroark@gmail.com>
2022-12-03 15:40:20 -0600

    add some dummy routes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App({ user, signOut }) {
   return (
     <div className="App">
       <Nav user={user} signOut={signOut} />
+      <Route path="/projects/:projectId/cells/:cellId">
+        {(params) => <div>{params?.cellId}</div>}
+      </Route>
+      <Route path="/projects/:projectId">
+        {(params) => <div>{params?.projectId}</div>}
+      </Route>
       <Route path="/projects">
         <Projects user={user} signOut={signOut} />
       </Route>
