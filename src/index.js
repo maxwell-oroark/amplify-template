e0b8b2d2cf1e5720759defd9b9ddb27008cd2987
Maxwell O'Roark <m.d.oroark@gmail.com>
2022-12-03 10:38:20 -0600

    remove jotai provider

--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// Jotai provider
-import { Provider } from "jotai";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 
